d35ebed25b6ba2fd907548052a9ddec9e5862879
sanjivaniayar <60250614+sanjivaniayar@users.noreply.github.com>
2020-02-11 10:10:47 +0530

    Add files via upload

---
+++ drive.sql
@@ -0,0 +1,66 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: drive
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `drive`
+--
+
+DROP TABLE IF EXISTS `drive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `drive` (
+  `d_id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(45) DEFAULT NULL,
+  `no_of_rsrcs` int(11) DEFAULT NULL,
+  `exp_req` tinyint(5) DEFAULT NULL,
+  `ctc` float DEFAULT NULL,
+  `join_crt` tinyint(2) DEFAULT NULL,
+  `bond` float DEFAULT NULL,
+  `position` tinyint(4) DEFAULT NULL,
+  `follow_up` date DEFAULT NULL,
+  `edu_crt` tinyint(3) DEFAULT NULL,
+  `cur_status` tinyint(6) DEFAULT NULL,
+  `nxt_act` tinyint(5) DEFAULT NULL,
+  `nxt_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ps_yr_req` int(11) DEFAULT NULL,
+  `gap_req` tinyint(2) DEFAULT NULL,
+  `creation_date` date DEFAULT NULL,
+  `tpoid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`d_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drive`
+--
+
+LOCK TABLES `drive` WRITE;
+/*!40000 ALTER TABLE `drive` DISABLE KEYS */;
+INSERT INTO `drive` VALUES (3,'ZENSAR',5,1,13000,1,1,0,'2020-01-02',0,4,4,'2020-02-02 18:30:00',2018,0,'2020-05-06',101),(4,'TCS',3,2,25000,1,2,0,'2020-02-03',0,4,2,'2020-04-04 18:30:00',2019,0,'2020-07-08',102),(5,'GREENLINE',8,2,30000,0,3,0,'2020-02-03',0,2,3,'2020-05-05 18:30:00',2020,0,'2020-07-08',102),(6,'GOOGLE',5,3,35000,1,1,0,'2020-04-05',1,1,1,'2020-06-06 18:30:00',2019,0,'2020-08-09',103),(7,'CISCO',3,2,40000,0,2,0,'2020-01-04',0,4,4,'2020-06-06 18:30:00',2016,0,'2020-08-09',104),(21,'INFOSYS',20,1,30000,1,1,0,'2020-02-02',0,4,4,'2020-03-02 18:30:00',2019,0,'2020-04-04',111);
+/*!40000 ALTER TABLE `drive` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-10 13:13:29
